8a31cdda6d3d147a0c9762c8bcd9c010ab6ffb42
true-2-wild <65196837+true-2-wild@users.noreply.github.com>
2021-01-29 08:47:16 -0700

    Add revised master and server maker files

---
+++ master.js
@@ -0,0 +1,10 @@
+const express = require("express");
+var app = express();
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
+
+app.get("/", function(req, res){
+  res.send("<h1>It works!</h1>");
+});
